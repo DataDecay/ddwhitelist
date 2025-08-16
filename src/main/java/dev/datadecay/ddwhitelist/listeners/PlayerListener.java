3549465a4aa65dc3266b10ef2737fcaed2a1df5b
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-08-16 14:27:35 -0400

    Update PlayerListener.java

--- src/main/java/dev/datadecay/ddwhitelist/listeners/PlayerListener.java
+++ src/main/java/dev/datadecay/ddwhitelist/listeners/PlayerListener.java
@@ -1,10 +1,12 @@
 package dev.datadecay.ddwhitelist.listeners;
 
 import dev.datadecay.ddwhitelist.DDWhitelist;
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.entity.Player;
 
 public class PlayerListener implements Listener {
 
@@ -15,20 +17,25 @@ public class PlayerListener implements Listener {
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        if (plugin.isWhitelistEnabled() &&
-            !event.getPlayer().hasPermission("ddwhitelist.allowjoin")) {
-
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (plugin.getConfig().getBoolean("enabled") && !player.hasPermission("ddwhitelist.allowjoin")) {
             event.setJoinMessage(null);
-            event.getPlayer().kickPlayer(plugin.getConfig().getString("messages.not_whitelisted"));
-            plugin.getServer().broadcastMessage("§c" + event.getPlayer().getName() + " tried to join but is not whitelisted!");
+
+            String msg = "%player_name% tried to join but is not whitelisted!";
+            if (plugin.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+                msg = PlaceholderAPI.setPlaceholders(player, msg);
+            }
+
+            plugin.getServer().broadcastMessage(msg);
+            player.kickPlayer("§cThis server is currently whitelisted!");
         }
     }
 
     @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
-        if (plugin.isWhitelistEnabled() &&
-            !event.getPlayer().hasPermission("ddwhitelist.allowjoin")) {
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if (plugin.getConfig().getBoolean("enabled") && !player.hasPermission("ddwhitelist.allowjoin")) {
             event.setQuitMessage(null);
         }
     }
