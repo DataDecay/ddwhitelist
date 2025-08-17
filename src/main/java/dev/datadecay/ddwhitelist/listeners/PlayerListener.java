e12ad4dc28e5feed58d6e49e74b1f7a3aeffc059
Samuel <samuel.w.wall@outlook.com>
2025-08-17 14:53:49 -0400

    - Actually put stuff in the correct package

--- src/main/java/dev/datadecay/ddwhitelist/listeners/PlayerListener.java
+++ src/main/java/dev/datadecay/ddwhitelist/listeners/PlayerListener.java
@@ -4,11 +4,20 @@ import dev.datadecay.ddwhitelist.DDWhitelist;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import me.clip.placeholderapi.PlaceholderAPI;
 
 public class PlayerListener implements Listener {
 
@@ -19,31 +28,26 @@ public class PlayerListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+        if (plugin.isWhitelistEnabled() && !(player.hasPermission(plugin.getGlobalPerm()) || player.hasPermission(plugin.getServerPerm()))) {
+            e.setJoinMessage(null);
+            String msg = plugin.getWhitelistMessage();
+            msg = PlaceholderAPI.setPlaceholders(player, msg);
+            Component componentMsg = LegacyComponentSerializer.legacyAmpersand().deserialize(msg);
+            plugin.getServer().broadcast(componentMsg);
+            Bukkit.getScheduler().runTaskLater(this.plugin, () -> player.kick(LegacyComponentSerializer.legacyAmpersand().deserialize(plugin.getKickMessage())), 2L);
 
-        if (plugin.getConfig().getBoolean("enabled") && !player.hasPermission("ddwhitelist.allowjoin")) {
-            event.setJoinMessage(null);
-
-            String msg = "%player_name% tried to join but is not whitelisted!";
-            if (plugin.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-                msg = PlaceholderAPI.setPlaceholders(player, msg);
-            }
-
-            // Convert to Component
-            Component broadcast = Component.text(msg, NamedTextColor.RED);
-            plugin.getServer().broadcast(broadcast);
-
-            // Kick player with Component
-            player.kick(Component.text("This server is currently whitelisted!", NamedTextColor.RED));
+            plugin.getLogger().info(player.getName() + " is not whitelisted and tried to join!");
         }
     }
 
+
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-        if (plugin.getConfig().getBoolean("enabled") && !player.hasPermission("ddwhitelist.allowjoin")) {
-            event.setQuitMessage(null);
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        if (plugin.isWhitelistEnabled() && !(player.hasPermission(plugin.getGlobalPerm()) || player.hasPermission(plugin.getServerPerm()))) {
+            e.setQuitMessage(null);
         }
     }
 }
