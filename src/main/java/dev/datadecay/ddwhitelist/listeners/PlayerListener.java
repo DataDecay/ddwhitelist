d8188fb5793594652e6187b7fb81e87205027f76
Samuel Wall <149903180+DataDecay@users.noreply.github.com>
2025-11-27 22:57:48 -0500

    Fix imports

--- src/main/java/dev/datadecay/ddwhitelist/listeners/PlayerListener.java
+++ src/main/java/dev/datadecay/ddwhitelist/listeners/PlayerListener.java
@@ -9,7 +9,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
@@ -47,4 +47,5 @@ public class PlayerListener implements Listener {
         }
     }
 }
+
 
